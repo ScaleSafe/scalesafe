a657ca8c92f97c5708134526f2d738ec73f6da80
tobinsouth <tobin.south@gmail.com>
2024-03-20 12:01:03 -0400

    Black autopep fixes

--- scalesafe/openai.py
+++ scalesafe/openai.py
@@ -2,11 +2,12 @@
 
 from .generic import GenericMonitor
 
+
 class OpenAIChatMonitor(GenericMonitor):
     """This is a monitor object to help to manage the compliance of OpenAI Chat API useage."""
+
     def __init__(self, api_key=None, location=None):
         super().__init__(api_key, location)
-
 
     def monitor(self, response, messages, api_key=None):
         """
@@ -19,24 +20,24 @@ class OpenAIChatMonitor(GenericMonitor):
             "model_end_time": response.created,
             "model_inputs": messages,
             "model_outputs": response.choices[0].message.content,
-            "openai_response_id": response.id
+            "openai_response_id": response.id,
         }
 
         return super().monitor(data, api_key)
-    
+
     def wrapper(client, model, messages, api_key=None, **kwargs):
         """This is a wrapper around the OpenAI chat completions.create method to monitor the useage."""
         monitor = OpenAIChatMonitor()
         response = client.chat.completions.create(
-            model=model,
-            messages=messages,
-            **kwargs
+            model=model, messages=messages, **kwargs
         )
         monitor.monitor(response, messages, api_key)
         return response
- 
+
+
 class OpenAIAssistantMonitor(GenericMonitor):
     """This is a monitor object to help to manage the compliance of OpenAI Assistant API useage."""
+
     def __init__(self, api_key=None, location=None):
         super().__init__(api_key, location)
 
@@ -50,7 +51,7 @@ class OpenAIAssistantMonitor(GenericMonitor):
             "model_end_time": response.created,
             "model_inputs": response.prompt,
             "model_outputs": response.choices[0].message.content,
-            "openai_response_id": response.id
+            "openai_response_id": response.id,
         }
 
         return super().monitor(data, api_key)
