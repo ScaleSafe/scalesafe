82e90e52e6554da7aa63f2e70fb4b42e72e7e612
tobinsouth <tobin.south@gmail.com>
2024-03-24 14:39:25 -0400

    Adding assistant example

--- tests/example_usage.py
+++ tests/example_usage.py
@@ -1,7 +1,11 @@
+import os; os.chdir('..')
 from scalesafe.openai import OpenAIChatMonitor
 from openai import OpenAI
 
 client = OpenAI()
+
+# %% Chat completion
+
 monitor = OpenAIChatMonitor()
 
 messages=[
@@ -27,3 +31,30 @@ OpenAIChatMonitor.wrapper(
     model="gpt-3.5-turbo",
     messages=messages
 )
+
+# %% Assistant actions
+
+assistant = client.beta.assistants.create(
+  name="Math Tutor",
+  instructions="You are a personal math tutor. Write and run code to answer math questions.",
+  tools=[{"type": "code_interpreter"}],
+  model="gpt-4-turbo-preview",
+)
+
+# Each thread needs to be monitored.
+thread = client.beta.threads.create()
+
+run = client.beta.threads.runs.create(
+  thread_id=thread.id,
+  assistant_id=assistant.id,
+  instructions="Please address the user as Jane Doe. The user has a premium account."
+)
+
+import time
+while run.status in ['queued', 'in_progress', 'cancelling']:
+  time.sleep(1) # Wait for 1 second
+  run = client.beta.threads.runs.retrieve(
+    thread_id=thread.id,
+    run_id=run.id
+  )
+
