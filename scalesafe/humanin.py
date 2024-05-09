17fab10001b003659519c50e0bc1bb7bedb995b0
tobinsouth <tobin.south@gmail.com>
2024-05-08 22:43:05 -0400

    Addign error raise template

---
+++ scalesafe/humanin.py
@@ -0,0 +1,7 @@
+from scalesafe.exceptions import OutOfComplianceError
+
+class TheLoop:
+    def run(self):
+        # Your loop logic here
+        raise OutOfComplianceError("This operation is out of compliance.")
+    """This is a prototype class for more advanced human in the loop features."""
